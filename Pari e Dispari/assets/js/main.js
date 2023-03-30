6b54bbb0d93aafaa9dda499ad0d159eaab6ea6a5
alecasentini <ale.casentini@gmail.com>
2023-03-30 17:41:58 +0200

    Aggiunta soluzione Pari e Dispari

--- Pari e Dispari/assets/js/main.js
+++ Pari e Dispari/assets/js/main.js
@@ -1,5 +1,23 @@
-let pari = document.getElementById ("pari")
-let dispari = document.getElementById ("dispari")
+function gioca(){
+    numberPlayer = parseInt (document.getElementById ("number-player").value);
 
-numberPlayer = parseInt (document.getElementById ("number-player").value)
+    let sceltaPari = document.getElementById ("pari").checked;
+    let sceltaDispari = document.getElementById ("dispari").checked;
 
+    numberCpu = Math.floor((Math.random() * 5) + 1);
+
+    let somma = numberPlayer + numberCpu;
+
+    let risultatoPari = somma % 2 == 0;
+
+    if ((risultatoPari && sceltaPari) || (!risultatoPari && sceltaDispari)){
+        document.getElementById("final-result").innerHTML =`Hai vinto!`
+    }
+
+    else{
+        document.getElementById("final-result").innerHTML =`Hai perso!`
+    }
+    
+    document.getElementById("number-cpu").innerHTML = "Numero del computer: " + numberCpu;
+    document.getElementById("somma").innerHTML = "Somma: " + somma;
+}
